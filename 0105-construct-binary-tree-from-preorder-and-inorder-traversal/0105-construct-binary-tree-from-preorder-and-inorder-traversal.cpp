2a6d10fd58d18371210a958d3079bb127595a0cf
PRADIP RATHOD <168919492+svpara@users.noreply.github.com>
2026-09-16 13:04:27 +0530

    Time: 0 ms (100%), Space: 27.4 MB (51.82%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,35 @@
+class Solution {
+ public:
+  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    unordered_map<int, int> inToIndex;
+    // that can be used of the another method it can be used 
+    // that are used the another method of the another leval of grant final tour 
+
+    for (int i = 0; i < inorder.size(); ++i)
+      inToIndex[inorder[i]] = i;
+
+    return build(preorder, 0, preorder.size() - 1, inorder, 0,
+                 inorder.size() - 1, inToIndex);
+            // this is set to the size of array 
+  }
+
+ private:
+  TreeNode* build(const vector<int>& preorder, int preStart, int preEnd,
+                  const vector<int>& inorder, int inStart, int inEnd,
+                  const unordered_map<int, int>& inToIndex) {
+     // tjhat can be sued 
+    if (preStart > preEnd)
+      return nullptr;
+
+    const int rootVal = preorder[preStart];
+    const int rootInIndex = inToIndex.at(rootVal);
+    const int leftSize = rootInIndex - inStart;
+
+    TreeNode* root = new TreeNode(rootVal);
+    root->left = build(preorder, preStart + 1, preStart + leftSize, inorder,
+                       inStart, rootInIndex - 1, inToIndex);
+    root->right = build(preorder, preStart + leftSize + 1, preEnd, inorder,
+                        rootInIndex + 1, inEnd, inToIndex);
+    return root;
+  }
+};
