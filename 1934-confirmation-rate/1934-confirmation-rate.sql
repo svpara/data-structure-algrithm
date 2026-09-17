4e14ae752ba7fe73e5771378b36715906c75c702
PRADIP RATHOD <168919492+svpara@users.noreply.github.com>
2026-09-17 13:13:11 +0530

    Time: 794 ms (38.43%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT 
+    s.user_id,
+    ROUND(
+        COALESCE(
+            AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0 END),
+            0
+        ),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
