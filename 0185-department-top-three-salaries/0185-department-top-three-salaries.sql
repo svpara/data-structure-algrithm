f7d411a80a8dcb13720376f0c0252aba421a2d1b
PRADIP RATHOD <168919492+svpara@users.noreply.github.com>
2026-09-19 12:59:11 +0530

    Time: 897 ms (95.98%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,16 @@
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT
+        *,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS rnk
+    FROM Employee
+) AS e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.rnk <= 3;
